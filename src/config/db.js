64151dff7433d2fc5d37805200f5d664496acfe5
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-02-29 16:55:59 +0530

    Inital Commit

---
+++ src/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  url: "mongodb+srv://ishaan_chadha:3VLR69gOWMrvf7P9@universityportalserverl.c04pgoi.mongodb.net/?retryWrites=true&w=majority",
+  database: "",
+  imgBucket: "photos",
+};
