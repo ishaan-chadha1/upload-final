2d9cc5b57099f9d73d82ab02810a964336ca6c4a
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-03-24 16:00:03 +0530

    Addition of Changes For Successful Deployment

--- src/server.js
+++ src/server.js
@@ -3,12 +3,18 @@ const express = require("express");
 const app = express();
 const initRoutes = require("./routes");
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ origin: "*" })); // Adjust the CORS policy as needed for your deployment
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
-module.exports = app; // Export the app
+module.exports = app;
+
+
+// Add this to the end of your server.js for local development only
+const port = process.env.PORT || 8080;
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`Running at localhost:${port}`);
+  });
+}
+
